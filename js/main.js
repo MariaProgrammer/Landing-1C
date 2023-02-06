b010d05a83ceee91f822fe22d68fdda83d7e230d
Maria <maryfrontend22@gmail.com>
2023-02-06 10:59:46 +0300

    form changed and css

--- js/main.js
+++ js/main.js
@@ -94,16 +94,16 @@ document.addEventListener('DOMContentLoaded', () => {
 		// console.log('headerHeight ' + headerHeight);
 	
 		if (scrollTop >=headerHeight && scrollTop < (height*2)) {
-			about.classList.add('.animation');
-			about.style.opacity = 1;
-			about.style.transition = `5s`;
+			about.classList.add('animation');
+			// about.style.opacity = 1;
+			// about.style.transition = `5s`;
 			
 			//  console.log('added ');
 			
 			} else {
-				about.classList.remove('.animation');
-				about.style.opacity = 0;
-			about.style.transition = `5s`;
+				about.classList.remove('animation');
+			// 	about.style.opacity = 0;
+			// about.style.transition = `5s`;
 
 			//  console.log('removed ');
 			}
@@ -208,70 +208,5 @@ window.addEventListener('scroll', () => {
 		}
 
 		//Настройка валидации
-
-		const validation = new JustValidate('.form');
-
-validation
-  .addField('.input-name', [
-    {
-      rule: 'minLength',
-      value: 3,
-    },
-    {
-      rule: 'maxLength',
-      value: 30,
-    },
-    {
-      rule: 'required',
-      value: true,
-      errorMessage: 'Введите имя!'
-    }
-  ])
-  .addField('.input-mail', [
-    {
-      rule: 'required',
-      value: true,
-      errorMessage: 'Email обязателен',
-    },
-    {
-      rule: 'email',
-      value: true,
-      errorMessage: 'Введите корректный Email',
-    },
-  ])
-  .addField('.input-tel', [
-    {
-      rule: 'required',
-      value: true,
-      errorMessage: 'Телефон обязателен',
-    },
-    {
-      rule: 'function',
-      validator: function() {
-        const phone = telSelector.inputmask.unmaskedvalue();
-        return phone.length === 10;
-      },
-      errorMessage: 'Введите корректный телефон',
-    },
-  ]).onSuccess((event) => {
-    console.log('Validation passes and form submitted', event);
-
-    let formData = new FormData(event.target);
-
-    console.log(...formData);
-
-    let xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          console.log('Отправлено');
-        }
-      }
-    }
-
-    xhr.open('POST', 'mail.php', true);
-    xhr.send(formData);
-
-    event.target.reset();
-  });
+		
+		
